3fe0b8b0e291d6706c1c566130a9ecdb762eb2e6
Zoe Guidry <oyezoey@gmail.com>
2022-11-02 00:09:29 -0500

    commit tree 50f586018ffea214790ce31057b870c4998eb8ed

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Your code here
+// First lab in js event module (the last is the tasklister one btw)
+const dodger = document.querySelector('#dodger');
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    } else if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    } else if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+})
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+    return;
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < (360 - dodger.style.width)) {
+      dodger.style.left = `${left + 1}px`;
+    }
+    return;
+}
+
+function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom > 0) {
+      dodger.style.bottom = `${bottom - 1}px`;
+    }
+    return;
+}
+
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom < (360 - dodger.style.height)) {
+      dodger.style.bottom = `${bottom + 1}px`;
+    }
+    return;
+}
